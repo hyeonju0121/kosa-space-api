065d5fcede418b72b15b3d37aad319cba09799e1
yu hyun ju <a01085249773@gmail.com>
2024-06-13 18:03:17 +0900

    Feat: DTO, DAO 생성

---
+++ src/main/java/com/mycompany/kosa_space/dto/EduCenter.java
@@ -0,0 +1,11 @@
+package com.mycompany.kosa_space.dto;
+
+import lombok.Data;
+
+@Data
+public class EduCenter {
+	private int ecno;
+	private String ecname;
+	private String ecaddress;
+	
+}
---
+++ src/main/java/com/mycompany/kosa_space/dto/TrainingRoom.java
@@ -0,0 +1,13 @@
+package com.mycompany.kosa_space.dto;
+
+import lombok.Data;
+
+@Data
+public class TrainingRoom {
+	private int trno;
+	private int ecno;
+	private String trname;
+	private int trcapacity;
+	private boolean trenable;
+	
+}
