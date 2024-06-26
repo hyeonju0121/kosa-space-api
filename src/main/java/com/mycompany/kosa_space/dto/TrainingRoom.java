c398830cccd1661c349077a59f6c1b10530f8eea
yu hyun ju <a01085249773@gmail.com>
2024-06-26 15:07:21 +0900

    Refactor: educenter, trainingroom DTO 수

--- src/main/java/com/mycompany/kosa_space/dto/TrainingRoom.java
+++ src/main/java/com/mycompany/kosa_space/dto/TrainingRoom.java
@@ -1,5 +1,9 @@
 package com.mycompany.kosa_space.dto;
 
+import java.util.Date;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
@@ -9,5 +13,12 @@ public class TrainingRoom {
 	private String trname;
 	private int trcapacity;
 	private boolean trenable;
+	
+	private MultipartFile trattach;
+	private String trattachoname;
+	private String trattachtype;
+	
+	private Date trcreatedat;
+	private Date trupdatedat;
 	
 }
