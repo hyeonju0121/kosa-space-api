c33083e9bf9f349c4412c2dbf1d7af222d1d7499
yu hyun ju <a01085249773@gmail.com>
2024-07-06 23:04:57 +0900

    Feat: Client IP 와 교육장 IP 일치 여부에 따른 예외처리 및 출결 시간 비교 기능 구현

--- src/main/java/com/mycompany/kosa_space/controller/AttendanceController.java
+++ src/main/java/com/mycompany/kosa_space/controller/AttendanceController.java
@@ -1,15 +1,47 @@
 package com.mycompany.kosa_space.controller;
 
-/*
-  	출석과 관련
-*/
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.kosa_space.dto.request.AttendanceTraineeRequestDTO;
+import com.mycompany.kosa_space.service.AttendanceService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@RequestMapping("/attendance")
 public class AttendanceController {
-
+	@Autowired
+	private AttendanceService attendanceService;
+	
+	// 교육생 입실 기능
+	@PostMapping("/checkin")
+	public void userCheckIn(@RequestBody AttendanceTraineeRequestDTO attendance,
+			HttpServletRequest reqeust) throws Exception {
+		String clientIP = reqeust.getHeader("clientIP");
+		
+		log.info("clientIP: " + clientIP);
+		
+		//log.info("mid: " + mid);
+		log.info("attendance: " + attendance.toString());
+		
+		attendanceService.checkin(clientIP, attendance);
+		
+		
+	}
+	
+	@GetMapping("/test/getClientIP")
+	public String getClientIP(HttpServletRequest request) {
+		
+		
+		String clientIP = request.getRemoteAddr();
+		return clientIP;
+	}
 }
--- src/main/java/com/mycompany/kosa_space/service/AttendanceService.java
+++ src/main/java/com/mycompany/kosa_space/service/AttendanceService.java
@@ -1,11 +1,76 @@
 package com.mycompany.kosa_space.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mycompany.kosa_space.dao.AttendanceDao;
+import com.mycompany.kosa_space.dto.request.AttendanceTraineeRequestDTO;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class AttendanceService {
+	
+	@Autowired
+	private AttendanceDao attendanceDao;
+	
+	public void checkin(String ip, AttendanceTraineeRequestDTO attendance) throws Exception {
+		// 교육장 IP 와 클라이언트 IP가 일치하지 않을 경우 에러처리 
+		String centerIP = "125.131.208.230";
+		String clientIP = ip;
+		
+		/*
+		if (clientIP == null) {
+			throw new RuntimeException("클라이언트 IP가 요청되지 않았습니다.");
+		} else {
+			if (centerIP.equals(ip.substring(3))) {
+				log.info("교육장 IP 와 클라이언트 IP가 일치합니다.");
+				clientIP = ip.substring(3);
+			} else {
+				throw new RuntimeException("교육장 IP 와 클라이언트 IP가 일치하지 않습니다.");
+			}
+		}
+		*/
+		
+		// 시간에 대한 처리 (09: 10 분 이후이면 사용자의 astatus 지각 처리로 업데이트)
+		// 09: 10 분 이전이면, 사용자의 astatus 를 정상 출결로 업데이트
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+
+        String absenceTimeStr = "09:10";
+        String requestTimeStr = "15:12";
+        //String requestTimeStr = attendance.getAttendancetime().substring(11, 16);
+        
+        log.info("absenceTimeStr: " + absenceTimeStr);
+        log.info("requestTimeStr: " + requestTimeStr);
+        
+
+        Date absenceTime = format.parse(absenceTimeStr);
+        Date requestTime = format.parse(requestTimeStr);
+
+        long difference = requestTime.getTime() - absenceTime.getTime();
+
+        int hours = (int) (difference / (1000 * 60 * 60));
+        int minutes = (int) ((difference / (1000 * 60)) % 60);
+        
+        if (minutes > 0 && hours >= 0) { // 지각 처리
+        	log.info("지각입니다.");
+        	
+        	
+        } else { // 정상 출결 처리 
+        	log.info("정상 출결입니다.");
+        }
+        
+        
+		String astatus = "";
+
+		
+		
+
+	}
 
 }
